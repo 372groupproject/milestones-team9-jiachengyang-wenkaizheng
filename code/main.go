0e7592b0304df8452f120e1a7737f110d18f1281
kv9699 <kv9699@gmail.com>
2020-03-21 21:06:25 -0700

    format

--- code/main.go
+++ code/main.go
@@ -1,36 +1,38 @@
 package main
 
 import (
-    "io/ioutil"
-    "encoding/json"
-    "fmt"
-    "os"
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"os"
 )
+
 type network struct {
-    Server      string   `json:"server"`
-    Server_port    int     `json:"server_port"`
-    Local_port    int     `json:"local_port"`
-    Password     string   `json:"password"`
-    Timeout      int      `json:"timeout"`
+	Server     string `json:"server"`
+	ServerPort int    `json:"server_port"`
+	LocalPort  int    `json:"local_port"`
+	Password   string `json:"password"`
+	Timeout    int    `json:"timeout"`
 }
 
-func file_get_contents(path string) ([]byte, error) {
+var configPATH = "./config.json"
+
+func readJson(path string) ([]byte, error) {
 	f, err := os.Open(path)
 	if err != nil {
 		return nil, err
 	}
 	return ioutil.ReadAll(f)
 }
- 
+
 func main() {
-    fmt.Println("Hello World")
+	fmt.Println("Hello World")
+
 	var c network
-	var content []byte
-	var err error
-	
-	content, err = file_get_contents("./config.json")
+
+	content, err := readJson(configPATH)
 	if err != nil {
-		fmt.Println("open file error: "+ err.Error())
+		fmt.Println("Cannot open " + configPATH + " : " + err.Error())
 		return
 	}
 	err = json.Unmarshal([]byte(content), &c)
@@ -38,6 +40,6 @@ func main() {
 		fmt.Println("ERROR: ", err.Error())
 		return
 	}
-	fmt.Println(c);
-	
+	fmt.Println(c)
+
 }
