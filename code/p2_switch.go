072a9714db02a808581a88887d9e8f0c9176c7e7
kv9699 <kv9699@gmail.com>
2020-04-05 19:36:15 -0700

    fmt & rename

--- code/p2_switch.go
+++ code/p2_switch.go
@@ -1,31 +1,38 @@
 // Copyright (c) 2016-2020, Regents of the University of Arizona.
 // Author:Wenkai Zheng<wenkaizheng@email.arizona.edu>
 //        JiaCheng Yang <jiachengyang@email.arizona.edu>
-// Control structure 
+// Control structure
 package main
+
 import (
 	"fmt"
 )
 
-func type5(i int){
-    switch i { 
-        case 0: 
-            fmt.Println("Type5 is 0") 
-        case 1: 
-            fmt.Println("Type5 is 1") 
-        default: 
-            fmt.Println("Type5 is default") 
-    } 
+func normalSwitch(i int) {
+	switch i {
+	case 0:
+		fmt.Println("normalSwitch: case 0")
+	case 1:
+		fmt.Println("normalSwitch:  case 1")
+	default:
+		fmt.Println("normalSwitch: default")
+	}
 }
-func type6 (variable int){
-    switch { 
-        case variable % 2 == 0: 
-            fmt.Println("Type6 is even")
-		default: 
-            fmt.Println("Typr6 is odd") 
-    }  
+func switchAsIfElseIF(variable int) {
+	switch {
+	case variable%2 == 0:
+		fmt.Println("variable is divisible by 2")
+	case variable%3 == 0:
+		fmt.Println("variable is divisible by 3")
+	case variable%5 == 0:
+		fmt.Println("variable is divisible by 5")
+	case variable%7 == 0:
+		fmt.Println("variable is divisible by 7")
+	default:
+		fmt.Println("variable is not divisible by 2, 3, 5 or 7")
+	}
 }
 func main() {
-    type5(0)
-    type6(0)
+	normalSwitch(0)
+	switchAsIfElseIF(0)
 }
