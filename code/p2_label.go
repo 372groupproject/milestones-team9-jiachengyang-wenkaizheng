b5d05d984b0b83b56910bb095b7b61bf8ce8b97d
kv9699 <kv9699@gmail.com>
2020-04-05 20:02:30 -0700

    added more

--- code/p2_label.go
+++ code/p2_label.go
@@ -1,10 +1,17 @@
 package main
+
 import "fmt"
-func main(){
+
+func main() {
 	var a int = 0
-	Loop: for a < 10{
+Loop:
+	for a < 10 {
 		fmt.Println(a)
 		a++
+		if a == 8 {
+			fmt.Println("Going to break")
+			break Loop
+		}
 		goto Loop
 	}
 }
