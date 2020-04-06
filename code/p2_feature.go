b816700aab36ab6028ad932a48682b741f1c6f94
kv9699 <kv9699@gmail.com>
2020-04-05 19:29:49 -0700

    fmt

--- code/p2_feature.go
+++ code/p2_feature.go
@@ -3,49 +3,49 @@
 //        JiaCheng Yang <jiachengyang@email.arizona.edu>
 // Hello World in different thread
 package main
+
 import (
-  //  "runtime"
-    "fmt"
-   
+	//  "runtime"
+	"fmt"
 )
 
-func Hello(){
-   for i:=0;i<10;i++{
-      fmt.Print("Hello ")
-      if i == 9{
-          ch3 <-0
-          break;
-      }
-      ch3 <-0
-      <- ch4
-      
-            
-    }
-   ch1 <- 0 
-}
-func World(){
-   <- ch3
-   for i:=0;i<10;i++{
-      fmt.Print("Wrold \n")
-      if i == 9{
-          break
-      }
-      ch4 <- 0
-      <- ch3
+func Hello() {
+	for i := 0; i < 10; i++ {
+		fmt.Print("Hello ")
+		if i == 9 {
+			ch3 <- 0
+			break
+		}
+		ch3 <- 0
+		<-ch4
 
-            
-    }
-   ch2 <- 0 
+	}
+	ch1 <- 0
 }
+func World() {
+	<-ch3
+	for i := 0; i < 10; i++ {
+		fmt.Print("Wrold \n")
+		if i == 9 {
+			break
+		}
+		ch4 <- 0
+		<-ch3
+
+	}
+	ch2 <- 0
+}
+
 var ch3 = make(chan int)
 var ch4 = make(chan int)
-var ch1 =  make(chan int)
+var ch1 = make(chan int)
 var ch2 = make(chan int)
+
 func main() {
-  
-  go Hello()
-  go World()
-  <-ch1
-  <- ch2
+
+	go Hello()
+	go World()
+	<-ch1
+	<-ch2
 
 }
