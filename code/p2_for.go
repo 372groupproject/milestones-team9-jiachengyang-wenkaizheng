6176acbc614de70a8f0974b9510d5a04d1eedae0
kv9699 <kv9699@gmail.com>
2020-04-05 19:31:44 -0700

    fmt & rename

--- code/p2_for.go
+++ code/p2_for.go
@@ -1,29 +1,31 @@
 // Copyright (c) 2016-2020, Regents of the University of Arizona.
 // Author:Wenkai Zheng<wenkaizheng@email.arizona.edu>
 //        JiaCheng Yang <jiachengyang@email.arizona.edu>
-// Control structure 
+// Control structure
 package main
+
 import (
 	"fmt"
 )
-func type1(){
-    var sum int = 0
-	for i:=0;i<10;i++{
-		sum+=i
+
+func normalFor() {
+	var sum int = 0
+	for i := 0; i < 10; i++ {
+		sum += i
 	}
-    fmt.Printf("Type1 sum is %d\n",sum)
+	fmt.Printf("Type1 sum is %d\n", sum)
 }
-func type2(){
-    var i int  = 0
-    var sum int = 0
-    for ;i<10;{
-       sum +=i
-       i++
-    }
-    fmt.Printf("Type2 sum is %d\n",sum)
+func forAsWhile() {
+	var i int = 0
+	var sum int = 0
+	for i < 10 {
+		sum += i
+		i++
+	}
+	fmt.Printf("Type2 sum is %d\n", sum)
 }
 
 func main() {
-	type1()
-    type2()
+	normalFor()
+	forAsWhile()
 }
