ef6a21b3ad0da70d75a209ff2285dbaad20e0cb8
kv9699 <kv9699@gmail.com>
2020-04-13 18:03:20 -0700

    update

--- code/p3_map.go
+++ code/p3_map.go
@@ -3,28 +3,28 @@ package main
 import "fmt"
 
 func mapOperation() {
-	m := make(map[int]string)
+	m := make(map[rune]string)
 
-	m[0] = "zero"
-	m[1] = "one"
-	m[2] = "two"
-	m[3] = "three"
-	m[4] = "four"
-	m[5] = "five"
+	m['零'] = "zero"
+	m['一'] = "one"
+	m['二'] = "two"
+	m['三'] = "three"
+	m['四'] = "four"
+	m['五'] = "five"
 	fmt.Println("Direct Println:", m)
 
-	v := m[0]
+	v := m['零']
 	fmt.Println("value of v: ", v)
 
 	fmt.Println("Length:", len(m))
 
-	delete(m, 0)
+	delete(m, '零')
 	fmt.Println("Direct Println:", m)
 
-	m[0] = "zero"
+	m['零'] = "zero"
 
 	for k, v := range m {
-		fmt.Printf("The key is %d and the value is %s\n", k, v)
+		fmt.Printf("The key is %c and the value is %s\n", k, v)
 	}
 	m1 := map[string]int{"zero": 0, "one": 1, "two": 2, "three": 3, "four": 4, "five": 5}
 	for k, v := range m1 {
