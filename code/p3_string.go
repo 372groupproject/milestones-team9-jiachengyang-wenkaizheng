0c85efe28711a0a3423ee4c421cbcc9684400f88
kv9699 <kv9699@gmail.com>
2020-04-13 17:38:14 -0700

    update

--- code/p3_string.go
+++ code/p3_string.go
@@ -21,7 +21,9 @@ func String() {
 	p += "abc"
 	fmt.Println(p)
 	fmt.Println(len(p))
-	p = "English中文日本語"
+	p = "English中文日本語は"
+	p2 := p[10]
+	fmt.Printf("%c\n", p2)
 	for i, c := range p {
 		fmt.Printf("%d , %c\n", i, c)
 
